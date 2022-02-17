e00ab7380ba32dfa1ae02000808a96a9bada518c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-18 02:40:38 +0600

    update expenses and balance

--- script.js/apply.js
+++ script.js/apply.js
@@ -1,11 +1,17 @@
 document.getElementById("cal-btn").addEventListener("click", function () {
     function getTotal() {
+        // Input value show
         let foodValue = document.getElementById("food-price").value;
         let rentValue = document.getElementById("rent-price").value;
         let clothesValue = document.getElementById("clothes-price").value;
+        // Input value sum
         let totalExpenses = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);
-
+        // Total Expenses
+        let incomeMoney = document.getElementById("income-money").value;
+        let ExpensesAmount = incomeMoney - totalExpenses;
+        // Update Expenses and Balance
         document.getElementById("total-expenses").innerText = totalExpenses;
+        document.getElementById("total-balance").innerText = ExpensesAmount;
     }
     getTotal();
 })
